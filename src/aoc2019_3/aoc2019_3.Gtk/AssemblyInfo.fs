187f607cdd6a9eff415fed1bdae5f829d50b8787
zakaluka <znprojects.apps@gmail.com>
2020-06-07 14:53:02 -0500

    doing aoc2019_3 as fabulous desktop app

---
+++ src/aoc2019_3/aoc2019_3.Gtk/AssemblyInfo.fs
@@ -0,0 +1,20 @@
+﻿namespace aoc2019_3.Gtk
+open System.Reflection
+open System.Runtime.CompilerServices
+
+[<assembly: AssemblyTitle("aoc2019_3.Gtk")>]
+[<assembly: AssemblyDescription("")>]
+[<assembly: AssemblyConfiguration("")>]
+[<assembly: AssemblyCompany("")>]
+[<assembly: AssemblyProduct("")>]
+[<assembly: AssemblyCopyright("")>]
+[<assembly: AssemblyTrademark("")>]
+
+// The assembly version has the format {Major}.{Minor}.{Build}.{Revision}
+
+[<assembly: AssemblyVersion("1.0.0.0")>]
+
+//[<assembly: AssemblyDelaySign(false)>]
+//[<assembly: AssemblyKeyFile("")>]
+
+()
