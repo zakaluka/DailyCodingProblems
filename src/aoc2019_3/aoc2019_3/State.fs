7bf45b8433f30bffa4da01dd54ce311166a7b01c
zakaluka <znprojects.apps@gmail.com>
2020-07-11 10:22:33 -0500

    refactored code into separate files

---
+++ src/aoc2019_3/aoc2019_3/State.fs
@@ -0,0 +1,29 @@
+﻿module aoc2019_3.State
+
+open Path
+open Wire
+
+let init() =
+  { ui =
+      { delayMs = 100
+        animate = true
+        input1 = ""
+        input2 = ""
+        lowestX = -10
+        highestX = 10
+        lowestY = -10
+        highestY = 10 }
+    pathOne = Path.empty
+    pathTwo = Path.empty
+    wireOne = Wire.empty
+    wireTwo = Wire.empty
+    intersectionPoints = [] },
+  []
+
+let update msg model =
+  match msg with
+  | CreatePaths -> failwith ""
+  | CreateWires -> failwith ""
+  | GetAllIntersectionPoints -> ""
+
+
---
+++ src/aoc2019_3/aoc2019_3/View.fs
@@ -0,0 +1,27 @@
+﻿// =============================================================================
+// Section for View
+// =============================================================================
+module  aoc2019_3.View
+
+open Fabulous
+open Fabulous.XamarinForms
+open Xamarin.Forms
+open Fabulous.SimplerHelpers
+
+let createButton txt msg dispatch =
+  Button.button [
+    Button.Text txt
+    Button.OnClick (fun _ -> dispatch msg )
+  ]
+
+let layout =
+  StackLayout.stackLayout [
+    StackLayout.Orientation StackOrientation.Vertical
+
+  ]
+
+let view model dispatch =
+  ContentPage.contentPage[
+    ContentPage.Content <|
+
+  ]
