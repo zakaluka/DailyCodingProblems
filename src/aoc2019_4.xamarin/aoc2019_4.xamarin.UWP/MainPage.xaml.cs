ba9203fa3591930285f3cbcf4e0f6b4d89f56526
zakaluka <znprojects.apps@gmail.com>
2020-08-07 10:08:51 -0500

    Initial commit for AOC 2019 Problem 4

---
+++ src/aoc2019_4.xamarin/aoc2019_4.xamarin.UWP/MainPage.xaml.cs
@@ -0,0 +1,14 @@
+﻿using Windows.Foundation;
+using Windows.UI.ViewManagement;
+
+namespace aoc2019_4.xamarin.UWP
+{
+    public sealed partial class MainPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            LoadApplication(new aoc2019_4.xamarin.App());
+        }
+    }
+}
